9f57926894c8bbb049dfa66fc0be7f206b62ca1d
PhoePA <pyaephyoaung.wcs@gmail.com>
2023-09-14 22:43:03 +0630

    added pagination

--- controllers/note.js
+++ controllers/note.js
@@ -6,10 +6,22 @@ const Note = require("../models/note");
 const { unlink } = require("../utils/unlink");
 
 exports.getNotes = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 6;
+  let totalNotes;
+  let totalPages;
   Note.find()
-    .sort({ createdAt: -1 })
+    .countDocuments()
+    .then((counts) => {
+      totalNotes = counts;
+      totalPages = Math.ceil(totalNotes / perPage)
+      return Note.find()
+        .sort({ createdAt: -1 })
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((notes) => {
-      return res.status(200).json(notes);
+      return res.status(200).json({notes, totalNotes, totalPages});
     })
     .catch((err) => {
       console.log(err);
